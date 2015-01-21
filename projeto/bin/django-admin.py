7f9bfeb5863f76132b03ba38b969b5aba47af97e
acens <acens@acens-desktop.(none)>
2015-01-21 14:47:28 -0300

    first commit

---
+++ projeto/bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/acens/projeto/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.7.3','django-admin.py'
+__requires__ = 'Django==1.7.3'
+__import__('pkg_resources').run_script('Django==1.7.3', 'django-admin.py')
