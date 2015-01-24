9cdfb736c0fcf318989393fe761b0b9b02151e0d
filipeherculano <filipeherculanorocha@gmail.com>
2015-01-23 23:12:13 -0300

    adicionado novo request para paginas de cadastro

--- projeto/sistema/core/views.py
+++ projeto/sistema/core/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
 	return render(request,'index.html',{})
+
+def profissionais(request):
+	return render(request, 'profissionais.html',{})
+
+def pacientes(request):
+	return render(request, 'paciente.html',{})
