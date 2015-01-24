35e4f7cba425ddf9afc23462e9f0d6542315a85a
filipeherculano <filipeherculanorocha@gmail.com>
2015-01-23 23:11:27 -0300

    adicionado novo url para link de paginas de cadastro

--- projeto/sistema/urls.py
+++ projeto/sistema/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'sistema.core.views.home', name='home'),
+    url(r'^profissionais/$', 'sistema.core.views.profissionais', name='profissionais'),
+    url(r'^pacientes/$', 'sistema.core.views.pacientes', name='pacientes'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
